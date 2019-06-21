247d8633eca0391028455558e9d9b361327cbe20
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-06-21 18:32:53 +0200

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis.
+	def full_title(page_title = '')
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_title
+		else 
+			page_title + " | " + base_title
+		end
+	end
 end
+
+
+def random_subdomain
+	("A".."Z").shuffle[0..7].join
