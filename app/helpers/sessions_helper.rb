b7e4dd917f160a941a77c0cf5da6ae255966da6d
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-08-16 13:47:48 +0200

    Allow users to update attributes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,13 +25,6 @@ module SessionsHelper
 		end
 	end
 
-	# # Returns the current logged-in user (if any)
-	# def current_user
-	# 	if session[:user_id]
-	# 		@current_user ||= User.find_by(id: session[:user_id])
-	# 	end
-	# end
-
 	# Returns true if the user is logged in, false otherwise
 	def logged_in?
 		!current_user.nil?
